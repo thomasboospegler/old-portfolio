bb615fd0223e6bd9055b6ad1d6fb7e885f54d630
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-15 23:55:31 -0400

    initial commit / organized project. Resolves #1

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prefer-stateless-function': 'off',
+  },
+};
