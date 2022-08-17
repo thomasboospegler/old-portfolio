16cf241415bd6c45b45962c309b459d5706e60f6
Thomas Boos Pegler <tboospegler@gmail.com>
2022-08-17 09:48:43 -0400

    Add material UI config

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,17 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import theme from './styles/Theme';
 import './index.css';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
---
+++ src/styles/Theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2ecc71',
+    },
+    // secondary: {
+    //   main: '',
+    // },
+  },
+});
+
+export default theme;
